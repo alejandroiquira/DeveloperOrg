f33676f3d51f4798e3e0a0c9502e253bf62e958d
Alejandro Iquira Aristizabal <alejandroiquira3@gmail.com>
2023-01-04 11:37:29 -0500

    order products and available products

--- force-app/main/default/lwc/availableProducts/availableProducts.js
+++ force-app/main/default/lwc/availableProducts/availableProducts.js
@@ -1,36 +1,75 @@
-import { LightningElement,wire,track,api} from 'lwc';  
+ import { LightningElement,wire,track,api} from 'lwc';  
  import {refreshApex} from '@salesforce/apex';  
- import getProducts from '@salesforce/apex/ProductUtility.getProductList';  
- import addProductsToOrder from '@salesforce/apex/ProductUtility.addProductsToOrder';  
- const COLS=[  
-   {label:'Name',fieldName:'Product2.Name', type:'text'},   
-   {label:'Price',fieldName:'UnitPrice', type:'currency'}  
- ];  
- export default class DataTableInLwc extends LightningElement {  
-   @api recordId;
-   cols=COLS;  
-   products;
-
-   @wire(getProducts,{orderId: '$recordId'}) productList (result){
-    if(result.data){
-      this.products=result.data;
-      console.log("productlist:"+result.data);
-      console.dir(result.data);
-    }
-     
-   };  
-
+ import gePriceBookProducts from '@salesforce/apex/availableProductLwc.gePriceBookProducts';  
+ import addProductsToOrder from '@salesforce/apex/availableProductLwc.addProductsToOrder';  
+ import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+ import {publish,MessageContext} from 'lightning/messageService';
+ import UPDATE_ORDER_PRODUCT_FILE from '@salesforce/messageChannel/updateOrderProducts__c';
  
-   addProducts(){  
-    console.log("addProducts"+this.recordId);
-     var selectedRecords =  
-      this.template.querySelector("lightning-datatable").getSelectedRows();  
-      addProductsToOrder({productList: selectedRecords, orderId: this.recordId})  
-     .then(result=>{  
-       return refreshApex(this.products);  
-     })  
-     .catch(error=>{  
-       alert('Error getting Available products'+JSON.stringify(error));  
-     })  
-   }  
+ const COLS=[ 
+    {label : 'Product Name',fieldName : 'productLink',type : 'url',
+        typeAttributes :{
+            label : {fieldName : 'Name'},
+            target : '_blank'
+        }
+    },  
+    {label:'Price',fieldName:'UnitPrice', type:'currency'}  
+ ];  
+
+ export default class DataTableInLwc extends LightningElement {
+    @api recordId;
+    cols=COLS;  
+    products;
+
+    @wire(MessageContext)
+    messageContext;
+  
+    @wire(gePriceBookProducts,{orderId: '$recordId'}) productList (result){
+      if(result.data){
+        this.products=result.data;
+        console.log("productlist:"+result.data);
+        console.dir(result.data);
+      }
+    };  
+   
+    searchKey ='';
+    handleOnChange(event){
+        let key=event.target.value;
+        console.log("Search key recordId:"+this.recordId);
+        
+      }
+
+    addProducts(){  
+      console.log("addProducts"+this.recordId);
+      var selectedRecords =  
+        this.template.querySelector("lightning-datatable").getSelectedRows();  
+        addProductsToOrder({productList: selectedRecords, orderId: this.recordId})  
+        .then(result=>{  
+            console.log("addProducts selectedRecords",selectedRecords);
+            const payload ={
+              productAddedMessage :'Product Added'
+          };
+          console.log("addProducts2");
+          publish(this.messageContext,UPDATE_ORDER_PRODUCT_FILE,payload);
+          this.showToast('Products added to the order.', result, 'Success', 'dismissable');
+          console.log("addProducts3");
+        
+      })
+      .catch(error=>{
+        //alert('Error adding products to the Order'+JSON.stringify(error)); 
+        this.showToast('Error Adding products:', error.body.message, 'error', 'dismissable'); 
+      })
+    }  
+
+   
+    showToast(title, message, variant, mode) {
+      const event = new ShowToastEvent({
+          title: title,
+          message: message,
+          variant: variant,
+          mode: mode
+      });
+      this.dispatchEvent(event);
+    }
+
  }  
--- force-app/main/default/lwc/orderProductsLwc/orderProductsLwc.js
+++ force-app/main/default/lwc/orderProductsLwc/orderProductsLwc.js
@@ -1,64 +1,88 @@
 import { LightningElement,wire,track,api} from 'lwc';  
   import {refreshApex} from '@salesforce/apex';  
-  import getOrderProducts from '@salesforce/apex/ProductUtility.getOrderProductsList';  
+  import getOrderProducts from '@salesforce/apex/orderProductLwc.getInitialProductsList'; 
+  import getRefreshedOrderProducts from '@salesforce/apex/orderProductLwc.getOrderProductsListImperative';   
+  import activateOrder from '@salesforce/apex/orderProductLwc.activateOrder';  
+  import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+  import { subscribe,MessageContext } from 'lightning/messageService';
+  import UPDATE_ORDER_PRODUCT_FILE from '@salesforce/messageChannel/updateOrderProducts__c';
   const COLS=[  
-  {label:'Name',fieldName:'Product2.Name', type:'text'},   
-  {label:'Quantity',fieldName:'Quantity', type:'integer'}  ,   
-  {label:'Unit Price',fieldName:'UnitPrice', type:'currency'}  ,   
-  {label:'Total Price',fieldName:'TotalPrice', type:'currency'}  
+    {label:'Name',fieldName:'productLink', type:'url',
+    typeAttributes :{
+          label : {fieldName : 'Name'},
+          target : '_blank'
+      }
+    },   
+    {label:'Quantity',fieldName:'Quantity', type:'number'}  ,   
+    {label:'Unit Price',fieldName:'UnitPrice', type:'currency'}  ,   
+    {label:'Total Price',fieldName:'TotalPrice', type:'currency'}  
   ];  
 
   export default class DataTableInLwc extends LightningElement {  
-  @api recordId;
-  cols=COLS;  
-  products;
-  listaproductos =[];
-
-  @wire(getOrderProducts, {orderid: '$recordId'}) wireproductList (result){
-    if(result.data){
-        this.products=result.data;
-    }
-    
-  };  
-
-
   
-  renderedCallback() {
-    console.log ("renderedCallback Record Id:"+ this.recordId);
-    getOrderProducts ({orderid:this.recordId}).then (result =>
+    @api recordId;
+    cols=COLS;  
+    products;
+
+
+    @wire(getOrderProducts, {orderid: '$recordId'}) wireproductList (result){
+      if(result.data){
+          this.products=result.data;
+      }
+    };  
+
+    counter =0;
+    subscription = null;
+
+    @wire(MessageContext)
+    messageContext;
+
+    connectedCallback(){
+        this.subscribeToMessageChannel();
+    }
+    subscribeToMessageChannel(){
+      console.log("subscribeToMessageChannel 1");
+        this.subscription = subscribe(
+            this.messageContext,
+            UPDATE_ORDER_PRODUCT_FILE,
+            (message) => this.handleMessage(message)
+        );
+    }
+    handleMessage(message){
+      
+      console.log("subscribeToMessageChannel 2");
+        if(message.productAddedMessage == 'Product Added'){
+          console.log("subscribeToMessageChannel 3");
+          getRefreshedOrderProducts({orderid : this.recordId})
+            .then(result=>{
+              this.products= result;
+              return refreshApex(this.products);  
+            })
+            .catch(error=>{
+              alert('Error refreshing order product Added'+JSON.stringify(error));  
+            });
+        }
+    }
+      
+    activateOrder(){
+      activateOrder({orderid : this.recordId}).then(result=>
         {
-            console.log ("Result UnitPrice:"+result[0].UnitPrice);
-            let items=[];
-            var orderProductsList = result;
-
-            console.log ("orderProductsList.length:"+orderProductsList.length);
-            /*
-            for (let i=0; i< orderProductsList.length; i++ )
-            {
-                var item ={
-                    
-                label:'ID',fieldName:'Id.Name', type:'text', value:   orderProductsList[i].Id,
-                label:'Name',fieldName:'Product2.Name', type:'text', value:   orderProductsList[i].Product2,
-                label:'Quantity',fieldName:'Quantity', type:'currency', value:   orderProductsList[i].Quantity,   
-                label:'Unit Price',fieldName:'UnitPrice', type:'currency', value:   orderProductsList[i].UnitPrice,
-                label:'Total Price',fieldName:'TotalPrice', type:'currency', value:   orderProductsList[i].TotalPrice    
-                }
-                items.push(item);
-                console.log("Item"+ orderProductsList[i].Id);
-            }
-            console.dir (items);
-            this.listaproductos= items;
-            console.dir (result);
-            */
-            //this.products=result;
-            }
-        );    
+          console.log("ActivateOrder2");
+          this.showToast('Order activated. Please refresh the page.', result, 'Success', 'dismissable');
+        }
+      ).catch(error=>{  
+        alert('Error activating order'+JSON.stringify(error));  
+      })
     }
 
-    
-    activateOrder(evt){
-        
-        
-    };
-  
- }  
+    showToast(title, message, variant, mode) {
+      const event = new ShowToastEvent({
+          title: title,
+          message: message,
+          variant: variant,
+          mode: mode
+      });
+      this.dispatchEvent(event);
+    }
+
+ }
